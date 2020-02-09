d2fb77ad345791a279bb01bad00e2d59939e1fc4
Karol <knoryskiewicz@gmail.com>
2020-02-09 14:50:45 +0100

    zadanie trzecie zrobione

--- src/main/java/tablice/trzy.java
+++ src/main/java/tablice/trzy.java
@@ -1,4 +1,20 @@
 package tablice;
 
+import java.lang.reflect.Array;
+
 public class trzy {
+    public static void main(String[] args) {
+        int array[]={13,22,45,54,68,31,47};
+        System.out.println(contains(array,31));
+    }
+
+
+    public static boolean contains(int[] array, int key){
+        for(int n:array) {
+            if (n == key)
+                return true;
+        }
+        return false;
+
+    }
 }
